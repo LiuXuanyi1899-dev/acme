ee90e1f1094be37d65eb2cd1797bc6bce7f34d20
xuanyi <lxy18996773375@gmail.com>
2024-12-09 18:43:17 +0800

    update

---
+++ jax_test.py
@@ -0,0 +1,5 @@
+import jax
+print("JAX devices:", jax.devices())
+# import tensorflow as tf
+# print("TensorFlow GPU devices:", tf.config.list_physical_devices('GPU'))
+# print(tf.config.experimental.list_physical_devices('GPU'))
